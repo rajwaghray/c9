f019452280e9e68ab939dd26f2bae305be53cd8c
rajwaghray <raj@16petals.com>
2016-03-14 06:37:17 +0000

    Changed User Login forms with Bootstrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #Adding for Devise: production.rb
+  config.action_mailer.default_url_options = { host: 'rkw3.herokuapp.com' }
 end
