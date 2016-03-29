68d563a3fde2176818594c8a39fabb5a7f9bb528
rajwaghray <raj@16petals.com>
2016-03-29 16:01:25 +0000

    integrated with aws with production.rb fixes - 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'rkw3.herokuapp.com' }
   
   #Adding for paperclip
-  config.paperclip.defaults = {
+  config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
