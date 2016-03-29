0bb74784ce5764cacfa1a7dd0a12e5a36c3fe282
rajwaghray <raj@16petals.com>
2016-03-29 16:00:06 +0000

    integrated with aws with production.rb fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,11 @@ Rails.application.configure do
   
   #Adding for paperclip
   config.paperclip.defaults = {
-    :storage => :s3
+    :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_KEY_ID']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
